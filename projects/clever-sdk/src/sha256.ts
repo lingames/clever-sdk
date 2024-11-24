25766fba36fcaf23560de9c093b93521cf9cd4c9
Aster <galaster@foxmail.com>
2024-11-24 17:47:32 +0800

    🧪 Support Google Adsense

--- projects/clever-sdk/src/sha256.ts
+++
@@ -1,7 +0,0 @@
-export declare namespace sha256 {
-    class sha256 {
-        constructor(algorithm: string, inputEncoding: string, options?: { encoding: string });
-        update(data: string): void;
-        getHash(outputEncoding: string): string;
-    }
-}
